635de9cd84e5fe594d6c16d08a7ec4e49232388b
Dustin Thomas <stdio@cptlobster.dev>
2024-12-05 14:02:24 -0600

    replace starter code, write simple text editor frontend

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,18 @@
+<textarea>
+
+</textarea>
+<style type="text/css">
+textarea {
+    resize: none;
+    overflow: auto;
+
+    color: var(--text);
+    background-color: var(--background);
+    height: 100%;
+    border: none;
+}
+
+textarea:focus {
+    outline: none;
+}
+</style>
